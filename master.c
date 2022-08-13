23ad4e711b5862d4826e53338b95f7859c9730ca
Andree14 <andrea.giacolono@gmail.com>
2022-08-13 21:54:37 +0200

    semaforo per masterbook info

--- master.c
+++ master.c
@@ -2,19 +2,6 @@
 
 #include "master.h"
 
-#define TEST_ERROR                                 \
-    if (errno)                                     \
-    {                                              \
-        fprintf(stderr,                            \
-                "%s:%d: PID=%5d: Error %d (%s)\n", \
-                __FILE__,                          \
-                __LINE__,                          \
-                getpid(),                          \
-                errno,                             \
-                strerror(errno));                  \
-        errno = 0;                                 \
-    }
-
 #define SO_USERS_NUM atoi(getenv("SO_USERS_NUM"))
 #define SO_NODES_NUM atoi(getenv("SO_NODES_NUM"))
 #define SO_REWARD atoi(getenv("SO_REWARD"))
@@ -31,11 +18,10 @@
 /* Definizione variabili ausiliarie */
 #define NODE_NAME "nodo"
 #define USER_NAME "user"
-#define ID_READY 0 /* figli pronti: padre puo` procedere */
-#define ID_GO 1    /* padre pronto: figli possono procedere */
 
 /* ID IPC Semaforo globale */
 int sem_nodes_users_id;
+int sem_masterbook_id;
 struct sembuf sops;
 char sem_n_id[3 * sizeof(int) + 1];
 
@@ -76,6 +62,8 @@ int main(int argc, char **argv, char **envp){
     char id_argument_sm_masterinfo[3 * sizeof(int) + 1]; /*id memoria condivisa masterbook*/
 
     char id_argument_sem_id[3 * sizeof(int) + 1]; /*id semaforo user e nodi*/
+    char id_argument_sem_masterbook_id[3 * sizeof(int) + 1]; /*id semaforo per scrivere e leggere su masterbook*/
+
 
     struct timespec* time;
     if (signal(SIGALRM, sig_handler)==SIG_ERR) {
@@ -116,14 +104,7 @@ int main(int argc, char **argv, char **envp){
     }
     user = (user_struct*)shmat(shared_users_id, NULL,0);
 
-
-    /* Creazione del semaforo per inizializzare user e nodi */ 
-    sem_nodes_users_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
-    if (sem_nodes_users_id == -1) {
-    	printf("Errore durante la creazione del semaforo users e nodes: %s\n",strerror(errno));
-        return -1;
-	}
-
+    /* Creazione memoria condivisa per info masterbook */
     shared_masterbook_info_id = shmget(IPC_PRIVATE, sizeof(masterbook_struct), 0600);
     if(shared_masterbook_info_id == -1){
         printf("Errore durante la creazione della shared memory: %s\n",strerror(errno));
@@ -132,18 +113,29 @@ int main(int argc, char **argv, char **envp){
 
     shd_masterbook_info = (masterbook_struct*)shmat(shared_masterbook_info_id, NULL,0);
     shd_masterbook_info->last_block_id = 0;
-    printf("bloc master inizializzato %d\n",shd_masterbook_info->last_block_id);
+
     /* Inizializzo il semaforo*/
-    shd_masterbook_info->sem_id_block = semget(getpid(), 1,IPC_CREAT | 0600);
-    if(shd_masterbook_info->sem_id_block == -1){
+    shd_masterbook_info->sem_masterbook = semget(getpid(), 1,IPC_CREAT | 0600);
+    if(shd_masterbook_info->sem_masterbook == -1){
         printf("Errore durante la creazione del semaforo delle informazioni sul masterbook: %s\n",strerror(errno));
         return -1;
     }
+    semctl(shd_masterbook_info->sem_masterbook, 0, SETVAL, 1);
     
-    printf("shd_masterbook_info->sem_id_block master: %d\n", shd_masterbook_info->sem_id_block);
 
-    semctl(shd_masterbook_info->sem_id_block, 0, SETVAL, 1);
+    /* Creazione del semaforo per inizializzare user e nodi */ 
+    sem_nodes_users_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
+    if (sem_nodes_users_id == -1) {
+    	printf("Errore durante la creazione del semaforo users e nodes: %s\n",strerror(errno));
+        return -1;
+	}
 
+    /* Creazione del semaforo per poter scrivere sul masterbook */ 
+    sem_masterbook_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
+    if (sem_masterbook_id == -1) {
+    	printf("Errore durante la creazione del semaforo users e nodes: %s\n",strerror(errno));
+        return -1;
+	}
 
     /* Inizializzazione semafori */
     sem_init();
@@ -155,19 +147,22 @@ int main(int argc, char **argv, char **envp){
     sprintf(id_argument_sm_masterinfo,"%d",shared_masterbook_info_id);
 
     sprintf(id_argument_sem_id, "%d", sem_nodes_users_id);
+    sprintf(id_argument_sem_masterbook_id,"%d",sem_masterbook_id);
 
     /* Argomenti per nodo*/
-    node_arguments[1] = id_argument_sm_nodes;
-    node_arguments[2] = id_argument_sem_id;
-    node_arguments[4] = id_argument_sm_masterbook;
-    node_arguments[5] = id_argument_sm_masterinfo;
+    node_arguments[0] = id_argument_sm_nodes;
+    node_arguments[1] = id_argument_sem_id;
+    node_arguments[3] = id_argument_sm_masterbook;
+    node_arguments[4] = id_argument_sm_masterinfo;
+    node_arguments[5] = id_argument_sem_masterbook_id;
 
     /* Argomenti per user*/
-    user_arguments[1] = id_argument_sm_users;
-    user_arguments[2] = id_argument_sm_nodes;
-    user_arguments[3] = id_argument_sm_masterbook;
-    user_arguments[5] = id_argument_sem_id;
-    user_arguments[6] = id_argument_sm_masterinfo;
+    user_arguments[0] = id_argument_sm_users;
+    user_arguments[1] = id_argument_sm_nodes;
+    user_arguments[2] = id_argument_sm_masterbook;
+    user_arguments[4] = id_argument_sem_id;
+    user_arguments[5] = id_argument_sm_masterinfo;
+    user_arguments[6] = id_argument_sem_masterbook_id;
 
 
 
@@ -203,7 +198,7 @@ void genera_nodi(char **envp)
             case 0:
                 printf("\nCreato nodo %d\n", getpid());
                 sprintf(node_id, "%d", i);
-                node_arguments[3] = node_id;
+                node_arguments[2] = node_id;
 
                 /*Inserisco dentro la memoria condivisa dei nodi il pid del nodo e l'id della coda di messaggi*/
                 nodes[i].pid = getpid();
@@ -243,7 +238,7 @@ void genera_utenti(char** envp)
             case 0:
                 printf("\nCreato user %d\n", getpid());
                 sprintf(user_id, "%d", i);
-                user_arguments[4] = user_id;
+                user_arguments[3] = user_id;
 
                 /*Inserisco dentro la memoria condivisa il pid dello user*/
                 user[i].pid=getpid();
@@ -269,6 +264,7 @@ void genera_utenti(char** envp)
 
 void sem_init(){
     semctl(sem_nodes_users_id, 0, SETVAL, SO_NODES_NUM+SO_USERS_NUM);
+    semctl(sem_masterbook_id, 0, SETVAL, 1);
 }
 
 void remove_IPC(){
